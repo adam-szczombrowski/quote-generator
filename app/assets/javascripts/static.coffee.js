7f9d2240cfc48c2b0ca6e90c21693fac5ea91947
adam-szczombrowski <adam.szczombrowski@gmail.com>
2016-02-17 12:04:55 +0100

    add more colours

--- app/assets/javascripts/static.coffee.js
+++ app/assets/javascripts/static.coffee.js
@@ -1,6 +1,16 @@
-quotes = [["A goal is not always meant to be reached, it often serves simply as something to aim at.","Brucee Lee"], ["To hell with circumstances. I create opportunities.", "Bruce Lee"], ["Do not pray for an easy life, pray for the strength to endure a difficult one.","Bruce Lee"], ["Success consists of going from failure to failure without loss of enthusiasm.", "Winston Churchill"], ["To improve is to change; to be perfect is to change often.", "Winston Churchill"], ["Attitude is a little thing that makes a big difference.","Winston Churchill"], ["A pessimist sees the difficulty in every opportynity, an optimist sees the opportunity in every difficulty.", "Winson Churchill"], ["The only way to have a friend is to be one.", "Ralph Waldo Emerson"], ["Always do what you are afraid to do","Ralph Waldo Emerson"], ["Nobody can bring you peace but yourself.","Ralph Waldo Emerson"]];
+quotes = [["A goal is not always meant to be reached, it often serves simply as something to aim at.","Brucee Lee"],
+ ["To hell with circumstances. I create opportunities.", "Bruce Lee"],
+ ["Do not pray for an easy life, pray for the strength to endure a difficult one.","Bruce Lee"],
+ ["Success consists of going from failure to failure without loss of enthusiasm.", "Winston Churchill"],
+ ["To improve is to change; to be perfect is to change often.", "Winston Churchill"],
+ ["Attitude is a little thing that makes a big difference.","Winston Churchill"],
+ ["A pessimist sees the difficulty in every opportynity, an optimist sees the opportunity in every difficulty.", "Winson Churchill"],
+ ["The only way to have a friend is to be one.", "Ralph Waldo Emerson"],
+ ["Always do what you are afraid to do","Ralph Waldo Emerson"],
+ ["Nobody can bring you peace but yourself.","Ralph Waldo Emerson"]];
 
-colors = ["#28191E", "#36181A", "#53181A","#762923","#A64630","#2C95A2","#364038","red"];
+colors = ["#28191E", "#36181A", "#53181A","#762923","#A64630","#2C95A2","#364038", "#177c19", "#069256", "#389874", "#7c1717",
+"#ffa700", "#FB2E01"];
 
 function change_color(colors){
   var col = colors[Math.floor(Math.random() * colors.length)];
